047d7acabd01f810e3326aa801127ac920d95c77
Sergey87 <serge.techno@gmail.com>
2022-05-09 18:02:24 +0300

    added HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 import sushiImage from '../../assets/sushi.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={styles.header}>
         <h1>Япона Кухня</h1>
-        <button>Корзина</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={sushiImage} alt='Блюда японской кухни' />
